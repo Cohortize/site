bfa3c585a35626f0ff784de89b8a74fdf20513d9
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-06-19 18:39:09 +0530

    just got a new idea, redesigning everything the previous design was good but too generic, like it was too simple and common, also it was not able to tell what it is supposed to, so i came with a new design in my dreams while i was sleeping, this one will hopefully signify the motive behind cohortize and also be unique

---
+++ src/components/homepage/path.tsx
@@ -0,0 +1,11 @@
+const Path = () => {
+    return (
+        <div className="min-h-screen w-full bg-black flex items-center px-8 lg:px-16 xl:px-20 py-16">
+            <div className="max-w-7xl w-full mx-auto flex flex-col lg:flex-row items-center justify-between gap-12 lg:gap-32 xl:gap-40">
+            
+            </div>
+        </div>
+    );
+};
+
+export default Path;
