18c076745c3416d9a780c56ff48dad856b617f5a
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-07-02 12:16:56 +0530

    made password reset frontend

--- src/app/components/global/forgot-password.tsx
+++ src/app/components/global/forgot-password.tsx
@@ -6,25 +6,25 @@ import { useState } from "react"
 import { InputOTP,
   InputOTPGroup,
   InputOTPSeparator,
-  InputOTPSlot
+  InputOTPSlot,
  } from "../ui/input-otp"
 
 export function ForgotPassword({
   className,
   ...props
 }: React.ComponentProps<"form">) {
-  const [otpSent, setOtpSent] = useState(false)
+  const [otpSent, setOtpSent] = useState("otpNotSent")
   
   const handleEmailSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    setOtpSent(true)
+    setOtpSent("otpSent")
   }
   
   const handleOtpSubmit = (e: React.FormEvent) => {
     e.preventDefault()
     console.log("OTP submitted")
   }
-  
+
   const emailInput = () => {
     return(
       <form className={cn("flex flex-col gap-6", className)} onSubmit={handleEmailSubmit} {...props}>
@@ -57,7 +57,7 @@ export function ForgotPassword({
           </p>
         </div>
         <div className="grid gap-6 justify-center">
-          <InputOTP maxLength={6}>
+          <InputOTP maxLength={6} inputMode="numeric" pattern="\d+">
             <InputOTPGroup>
               <InputOTPSlot index={0} />
               <InputOTPSlot index={1}/>
@@ -70,7 +70,7 @@ export function ForgotPassword({
               <InputOTPSlot index={5}/>
             </InputOTPGroup>
           </InputOTP>
-          <Button type="submit" className="w-full bg-[rgb(44,44,44)] hover:bg-[rgb(48,48,48)] cursor-pointer">
+          <Button type="submit" className="w-full bg-[rgb(44,44,44)] hover:bg-[rgb(48,48,48)] cursor-pointer" onClick={() => setOtpSent("otpGot")}>
             Submit OTP
           </Button>
         </div>
@@ -78,7 +78,40 @@ export function ForgotPassword({
     );
   }
   
-  return (
-    otpSent ? otpInput() : emailInput()
-  );
+  const ResetPassword = () => {
+    return(
+          <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold text-[#b6b4b4]">Enter new password</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter new password for your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email" className="text-[#b6b4b4]">Password</Label>
+          <Input id="password" className="border border-white/20" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password" className="text-[#b6b4b4]">Confirm Password</Label>
+          </div>
+          <Input id="conPassword" type="password" className="border border-white/20" required />
+        </div>
+        <Button type="submit" className="w-full bg-[rgb(44,44,44)] hover:bg-[rgb(48,48,48)] cursor-pointer">
+          Change Password
+        </Button>
+      </div>
+    </form>
+    )
+  }
+  
+  switch(otpSent){
+    case "otpNotSent":
+      return emailInput()
+    case "otpSent":
+      return otpInput()
+    case "otpGot":
+      return ResetPassword()
+  }
 }
